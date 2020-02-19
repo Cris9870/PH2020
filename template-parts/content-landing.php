3ccf91e2dca1940427d0b3bed1163c67c67ee127
Isaac Gomez <isaacgomez@MacBook-Air.localdomain>
2020-02-19 17:30:22 -0500

    contenedor para estructuras del landing page

--- template-parts/content-landing.php
+++ template-parts/content-landing.php
@@ -1,3 +1,6 @@
 <?php get_header(); ?>
 
+	<?php get_template_part('template-parts/landing-sections/content', 'hero-section');?>
+	<?php get_template_part('template-parts/landing-sections/content', 'trip-advisor-section');?>
+
 <?php get_footer();?>
